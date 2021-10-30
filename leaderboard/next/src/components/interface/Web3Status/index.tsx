a9af825b5d997b4200286953b298a4903fc2e153
rxchard <40161763+rxchard@users.noreply.github.com>
2021-10-30 17:38:11 +0200

    refactor(leaderboard:next): move connector definition to util

--- leaderboard/next/src/components/interface/Web3Status/index.tsx
+++ leaderboard/next/src/components/interface/Web3Status/index.tsx
@@ -4,15 +4,13 @@ import { AlertTriangle } from 'react-feather'
 import tw from 'twin.macro'
 import { Avatar } from '../Avatar'
 import { makeShortAddress } from '../../../modules/util/address'
-import { InjectedConnector } from '@web3-react/injected-connector'
+import { injected } from '../../../modules/util/connectors'
 
 const StatusFrame = tw.div`inline-flex flex-row items-center pt-1 pb-1 pl-4 pr-4 space-x-2 rounded-xl bg-mandy-500 hover:cursor-pointer flex-nowrap`
 
 const StatusText = tw.p`text-white`
 
 const NetworkErrorIcon = tw(AlertTriangle)`w-4 h-4 text-white`
-
-const injected = new InjectedConnector({ supportedChainIds: [1] })
 
 export const Web3Status: React.FC = () => {
   const { account, error, activate } = useWeb3React()
