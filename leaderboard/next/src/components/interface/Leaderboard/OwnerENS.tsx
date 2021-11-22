141e06e20b99902d6d48043d1ec67230ec448809
rxchard <40161763+rxchard@users.noreply.github.com>
2021-11-22 16:33:19 +0100

    fix(leaderboard:next): show custom name again

--- leaderboard/next/src/components/interface/Leaderboard/OwnerENS.tsx
+++ leaderboard/next/src/components/interface/Leaderboard/OwnerENS.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { useENS } from '@/modules/hooks/web3'
 import { Owner, OwnerProps } from './Owner'
 
-export const OwnerENS: React.FC<OwnerProps> = ({ address, ...props }) => {
-  const { name } = useENS(address)
+export const OwnerENS: React.FC<OwnerProps> = ({ address, name, ...props }) => {
+  const { name: ensName } = useENS(address)
 
-  return <Owner {...props} name={name} address={address} />
+  return <Owner {...props} name={name || ensName} address={address} />
 }
